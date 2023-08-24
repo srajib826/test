44c83a8a09a5ef3ea9f56a0177f2cf11db0bf087
Rajib sarkar <srajib826@gmail.com>
2023-08-24 17:28:38 +0530

    median of 5 numbers

--- first_file.py
+++ first_file.py
@@ -1,7 +1,47 @@
-print("Hi")
-print("my name is raja")
-print('anything')
-print('anything 2')
-print("++++++")
-print('******')
-print('git_ignore')
+a=int(input('enter a number'))
+b=int(input('enter a number'))
+c=int(input('enter a number'))
+d=int(input('enter a number'))
+e=int(input('enter a number'))
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+min =a
+if b< min :
+    min=b
+if c< min :
+    min=c
+if d< min :
+    min=d
+if e< min :
+    min=e    
+#print(min)    
+min2=float('inf')
+
+if a>min and a< min2 :
+    min2=a
+if b>min and b< min2 :
+    min2=b
+if c>min and c< min2 :
+    min2=a
+if d>min and d< min2 :
+    min2=a
+if e>min and e< min2 :
+    min2=e
+
+#print(min2)
+min3=float('inf')
+if a>min2 and a< min3 :
+    min3=a
+if b>min2 and b< min3 :
+    min3=b
+if c>min2 and c< min3 :
+    min3=c
+if d>min2 and  d< min3 :
+    min3=d
+if e>min2 and e< min3 :
+    min3=e
+
+print(min3)
