4da57fc6a595caa7366d22b7bb008b4ad09f788a
Rajib sarkar <srajib826@gmail.com>
2023-08-31 20:10:59 +0530

    some modification

--- data.py
+++ data.py
@@ -5,5 +5,6 @@ with open('test_data_collection.txt',"r") as file:
     lines = file.readlines()
     for line in lines:
         d = json.loads(line)
+        print(np.array(d['answer']['rp_y']))
         print(np.array(d['answer']['rp_y']).shape)
         break
